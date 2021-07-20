1bcb968fbc202d2daf1a8ecfb0706aaa7fd89d17
amireshratifar <amir.eshratifar@clarifa.com>
2021-07-19 20:14:38 -0400

    minor fix.

--- build_trt_plugins.sh
+++ build_trt_plugins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Build YoloV5 TensorRT Plugins
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install --yes git make cmake
-mkdir -p ./yolov5/build
+#apt-get update
+#DEBIAN_FRONTEND=noninteractive apt-get install --yes git make cmake
+mkdir -p ./tensorrt-plugins/yolov5/build
 cd $_ && cmake .. && make
 mkdir -p /opt/tritonserver/trt_plugins
 mv libyoloplugin.so $_
