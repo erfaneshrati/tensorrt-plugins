316f03180e7ed86656b490b0517ed7c16e102048
amireshratifar <amir.eshratifar@clarifa.com>
2021-07-19 20:06:51 -0400

    TensorRT plugin for YoloV5.

---
+++ build_trt_plugins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Build YoloV5 TensorRT Plugins
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install --yes git make cmake
+mkdir -p ./yolov5/build
+cd $_ && cmake .. && make
+mkdir -p /opt/tritonserver/trt_plugins
+mv libyoloplugin.so $_
+# Clean up
+apt-get remove --purge --yes git make cmake
